069d1b3a784e1c01669874de443dc46960d82fbf
Paul <maxishoes@gmail.com>
2026-06-03 15:02:14 +0000

    feat(tv-ads-android): url builder + playback-plan mapping (unit-tested)

---
+++ android-tv-ads/app/src/test/kotlin/pl/zira/tvads/PlaybackPlanTest.kt
@@ -0,0 +1,39 @@
+package pl.zira.tvads
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import pl.zira.tvads.model.AdVideo
+import pl.zira.tvads.model.Playlist
+import pl.zira.tvads.net.UrlBuilder
+import pl.zira.tvads.player.PlaybackPlan
+import pl.zira.tvads.player.RepeatMode
+
+class PlaybackPlanTest {
+    private fun pl(mode: String, repeat: String?) = Playlist(
+        "v", mode, repeat, true, 0,
+        listOf(AdVideo("a", "/video/a", 0), AdVideo("b", "/video/b", 1)),
+    )
+
+    @Test fun absoluteUrl() {
+        assertEquals("http://192.168.1.5:17893/video/a",
+            UrlBuilder.absolute("http://192.168.1.5:17893", "/video/a"))
+    }
+
+    @Test fun sequentialPlaysAllRepeatAll() {
+        val plan = PlaybackPlan.from(pl("sequential", null), "http://h:1")
+        assertEquals(listOf("http://h:1/video/a", "http://h:1/video/b"), plan.urls)
+        assertEquals(RepeatMode.ALL, plan.repeatMode)
+    }
+
+    @Test fun repeatOnePlaysOnlyTargetRepeatOne() {
+        val plan = PlaybackPlan.from(pl("repeat-one", "b"), "http://h:1")
+        assertEquals(listOf("http://h:1/video/b"), plan.urls)
+        assertEquals(RepeatMode.ONE, plan.repeatMode)
+    }
+
+    @Test fun repeatOneWithMissingTargetFallsBackToAll() {
+        val plan = PlaybackPlan.from(pl("repeat-one", "zzz"), "http://h:1")
+        assertEquals(listOf("http://h:1/video/a", "http://h:1/video/b"), plan.urls)
+        assertEquals(RepeatMode.ALL, plan.repeatMode)
+    }
+}
